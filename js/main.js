78289d5c0e2df939751807e867476bed0279fd06
Marcos Aaron Castro Rivas <maracastror@gmail.com>
2023-08-18 11:06:04 -0600

    add: Se añade información al acordeon.

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
   if(window.location.href.indexOf('index') > -1) {
     var posts = [
       {
-        title: 'Titulo 1',
+        title: 'Mi experiencia con Python',
         date: 'Publicado el ' + moment().day() + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY'),
         content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quod aperiam cupiditate inventore maiores cumque quae atque neque, architecto similique distinctio et saepe eum consequuntur aliquid provident corporis adipisci veritatis incidunt quam. Quae nam quibusdam inventore! Sint quos ratione distinctio quo illo maxime, voluptate soluta earum ipsum temporibus doloremque, nisi magni incidunt, laboriosam debitis veritatis at. Impedit voluptates explicabo laborum perferendis. Omnis in dolore totam molestiae ea dignissimos ut quae natus aliquid quisquam alias soluta a quas tempore adipisci, pariatur quo. Dolor, fugit dolores? Veniam aliquid maxime eveniet maiores eos nobis unde minima quasi vel omnis quae nemo ea autem optio voluptas aliquam magni saepe libero nisi impedit itaque fugit, expedita eum. Voluptatum saepe corrupti velit, suscipit dolores deserunt consequuntur!'
       },
